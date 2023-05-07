2f17d3ac4f657240479dabf43d18adb1300ca941
Kate Demchuk <kate.v.demchuk@gmail.com>
2023-05-07 23:41:43 +0200

    Minor concat fix

--- src/Option.js
+++ src/Option.js
@@ -2,9 +2,9 @@ import './Option.css';
 
 export default function Option({content, selectable=false, select, isSelected=false}) {
   let optionClassName = 'Option';
-  if (selectable) optionClassName += ' ' + 'Option-selectable';
-  if (content.status) optionClassName += ' ' + `Option-${content.status}`;
-  if (isSelected) optionClassName += ' ' + 'Option-chosen';
+  if (selectable) optionClassName += ' Option-selectable';
+  if (content.status) optionClassName += ` Option-${content.status}`;
+  if (isSelected) optionClassName += ' Option-chosen';
 
   return (
     <div className={optionClassName} onClick={select}>
