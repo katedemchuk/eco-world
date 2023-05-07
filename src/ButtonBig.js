d4369b6c23644803300f31ef782954ad4333b7ab
Kate Demchuk <kate.v.demchuk@gmail.com>
2023-05-07 23:41:32 +0200

    Button disabled state

--- src/ButtonBig.js
+++ src/ButtonBig.js
@@ -1,7 +1,7 @@
 import './ButtonBig.css';
 
-export default function ButtonBig({children, action, doBlink=false}) {
+export default function ButtonBig({children, action, doBlink=false, disabled}) {
   return (
-    <button onClick={action} className={'ButtonBig ' + (doBlink ? 'ButtonBig-blink' : '')}>{children}</button>
+    <button onClick={action} className={'ButtonBig ' + (doBlink ? 'ButtonBig-blink' : '')} disabled={disabled}>{children}</button>
   );
 }
