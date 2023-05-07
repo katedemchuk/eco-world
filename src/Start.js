f9bf26e921aec16c82e5d75e25076a34ea0f3c1b
Kate Demchuk <kate.v.demchuk@gmail.com>
2023-05-07 18:16:55 +0200

    Add Start component

---
+++ src/Start.js
@@ -0,0 +1,12 @@
+import './Start.css';
+import ButtonBig from './ButtonBig';
+
+export default function Start() {
+  return (
+    <div class="Start">
+      <h1 class="Start-title">Еко вибір</h1>
+      <p class="Start-description">Ви потрапили в альтернативну реальність, де правлять екоактивісти. <b>Чи зможете стати частиною общини та асимілюватися?</b></p>
+      <ButtonBig>Почати гру</ButtonBig>
+    </div>
+  );
+}
